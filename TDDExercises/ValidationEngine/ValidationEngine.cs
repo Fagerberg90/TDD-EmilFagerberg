43fbe22844131a9b224a3fdde20ec781613975d2
Emil <fagerberg_90@hotmail.com>
2017-05-23 12:53:18 +0200

    ValidationEngine class added and ValidationTest class + reference

---
+++ TDDExercises/ValidationEngine/ValidationEngine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ValidationEngine
+{
+    public class ValidationEngine
+    {
+        public bool IsValid { get; set; }
+        public bool Validate(string email)
+        {
+            if (email == null)
+            {
+                return false;
+            }
+            var regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            var match = regex.Match(email);
+            return match.Success;
+        }
+    }
+}
--- TDDExercises/ValidationEngineTests/ValidationTests.cs
+++ TDDExercises/ValidationEngineTests/ValidationTests.cs
@@ -10,6 +10,42 @@ namespace ValidationEngineTests
     [TestFixture]
     public class ValidationTests
     {
+        [Test]
+        public void ValidateValidEmail()
+        {
+            var sut = new ValidationEngine.ValidationEngine();
 
+            sut.IsValid = sut.Validate("hello@hotmail.com");
+
+            Assert.IsTrue(sut.IsValid);
+        }
+
+        [Test]
+        public void ValidateEmailThatIsMissingAtSign()
+        {
+            var sut = new ValidationEngine.ValidationEngine();
+
+            sut.IsValid = sut.Validate("hellohotmail.com");
+
+            Assert.IsFalse(sut.IsValid);
+        }
+        [Test]
+        public void ValidateEmailThatIsMissingDot()
+        {
+            var sut = new ValidationEngine.ValidationEngine();
+
+            sut.IsValid = sut.Validate("hello@hotmailcom");
+
+            Assert.IsFalse(sut.IsValid);
+        }
+        [Test]
+        public void ValidateEmailThatIsNull()
+        {
+            var sut = new ValidationEngine.ValidationEngine();
+
+            sut.IsValid = sut.Validate(null);
+
+            Assert.IsFalse(sut.IsValid);
+        }
     }
 }
